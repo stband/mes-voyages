0ca3d9b3d17b483098a1f0f42acbc9d4aa66b110
st <st.trismegistus@gmail.com>
2025-01-14 15:18:00 +0100

    ajout : config twig

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AccueilController
+ *
+ * @author Esteban
+ */
+class AccueilController extends AbstractController{
+    #[Route('/', name: 'accueil')]
+    public function index(): Response {
+       return $this->render("pages/accueil.html.twig");
+    }
+}
